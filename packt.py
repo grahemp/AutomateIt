0f9f2b3b2f4a41726ca262772f442a08b7ea7d32
grahemp <pmeharg@gmail.com>
2017-09-14 15:15:31 -0500

    update packt.py after botched merge

--- packt.py
+++ packt.py
@@ -1,6 +1,5 @@
 #
 import bs4, urllib2
-<<<<<<<
 
 def checkforbook():
     booklist = file('mybooks.txt')
@@ -12,10 +11,6 @@ def checkforbook():
 
     return found
 
-
-=======
-
->>>>>>>
 page = urllib2.urlopen("https://www.packtpub.com/packt/offers/free-learning")
 soup = bs4.BeautifulSoup(page, "lxml")
 #Making the soup
