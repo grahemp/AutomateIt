cc58926bc0ad4e77dc3f3615739e7011fae9742e
grahemp <pmeharg@gmail.com>
2017-10-04 21:51:01 -0500

    corrected UnicodeDecodeError error

--- packt.py
+++ packt.py
@@ -1,21 +1,34 @@
 #
-import bs4, urllib2
+import bs4, urllib2, sys
+# import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 def checkforbook():
+    # this function will open the file with the list of books
+    # and search through the list.
+    # If the book is found (in other words, I have that book)
+    # it returns True 
+    # if not found, it returns false.
+    
     booklist = file('mybooks.txt')
     found = False
     for line in booklist:
-        if dotd.text.strip() in line:
+        # print line
+        if dotdTitle in line:
             found = True
             break
-
     return found
+
 
 page = urllib2.urlopen("https://www.packtpub.com/packt/offers/free-learning")
 soup = bs4.BeautifulSoup(page, "lxml")
+
 #Making the soup
 dotd = soup.find(class_="dotd-title")
-print dotd.text.strip()
+dotdTitle = dotd.text.strip()
+# print dotd.text.strip()
+print "The name of the free book for todday is: " + dotdTitle
 
 # if dotd.text.strip() in open('list.txt').read():
 #     print "Already have this one!"
@@ -24,3 +37,4 @@ if gotit:
     print "I have this one."
 else:
     print "This is a new one!"
+
